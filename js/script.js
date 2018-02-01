1a64fa91320ac18fd9365dcd3e73eb40f3f508c1
kimi s <sport1.kimi@gmail.com>
2018-02-01 16:55:51 +0530

    Coming Soon page
    Coming soon page developed by me, for my photography website Moments By
    Kimi features a countdown upto a certain date in future. It also
    features a typewriter effect text  that features as welcome message as
    well as a punchline.

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+    
+    
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+        
+         $('.my-countdown').countdown({
+            until: $.countdown.UTCDate(
+                -7, 2019, 8, 21, 0, 0, 0
+            ),
+             layout: $('.my-countdown').html()
+//             layout: '<span class="countdown-section"><span class="countdown-amount">{dn}       <span class="countdown-sep">{sep}</span></span><span class="countdown-period">{dl}</span></span>'+
+//                '<span class="countdown-section"><span class="countdown-amount">{hn} <span class="countdown-sep">{sep}</span></span><span class="countdown-period">{hl}</span></span>'
+        });
+    };
+
